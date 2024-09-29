d86f11193698c12e55c7ba6babd0a86be42a7d61
Aditya <adis23444@gmail.com>
2024-09-29 17:03:32 +0530

    added toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import TestMenuComponent from "./navbarcomponents/TestMenuComponent";
 import Clock from "./navbarcomponents/Clock";
+import ClockToggle from "./buttons/ClockToggle";
+import { ToggleContext } from "./ToggleContext";
+
 
 const App = () => {
-  const [isClock,setIsClock] = useState(true);
+
+const [isClock,setIsClock] = useState(false);
 
   return (
-  <>
-    <div></div>
+  <ToggleContext.Provider value={{isClock,setIsClock}}>
+    <div><ClockToggle/></div>
     <div className="w-full h-screen grid place-content-center">
       {isClock===true? <Clock/> :
       <TestMenuComponent/>}
     </div>
-  </> );
+  </ToggleContext.Provider> );
 };
 
 export default App;
