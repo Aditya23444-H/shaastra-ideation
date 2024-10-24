409f32aba6d1cede28e14a41ed2b8b6b27b491ca
swaraj-u <swarajrupadhyay@gmail.com>
2024-10-24 21:04:25 +0530

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,12 @@ import About from "./pages/About";
 import Timer from "./pages/Timer";
 import Footer from "./pages/Footer";
 import NavbarComponent from "./pages/NavbarComponent";
-import { div } from "framer-motion/client";
+
 
 const App = () => {
   const [isClock,setIsClock] = useState(false);
   const ref = useRef(null);
+  const refreshRef = useRef(null);
   const [hasScrolled,setHasScrolled] = useState(false);
 
   useEffect(()=>{
@@ -24,9 +25,15 @@ const App = () => {
       }
     }
     window.addEventListener('scroll', handleScroll);
+
+    // ref.current.style.transform = "none";
+
+    // console.log("useEffect ran!.", refreshRef.current);
     return () => {
       window.removeEventListener('scroll', handleScroll);
     }
+
+   
   },[])
 
   const {scrollYProgress} = useScroll({
@@ -40,7 +47,6 @@ const App = () => {
     <section ref={ref} className="relative h-[400vh]"
     style={{ scrollBehavior: "smooth" }}>
       <div className="flex sticky top-0 h-screen w-max bg-p6">
-        
           <motion.div
             variants={{
               hidden: { opacity:0, y:"100vh"},
@@ -48,20 +54,20 @@ const App = () => {
                 opacity:1,
                 y:0,
                 transition:{
-                  duration:1, delay:4, type:"spring"
+                  duration:1,
+                  delay:4,
+                  type:"spring"
                 }
               }
             }}
-            style={hasScrolled?{transform:"translateX(-10%)"}:{x, left:"10%"}}
+            style={hasScrolled?{transform:"translateX(-12.5%)"}:{x, left:"10%"}}
             initial="hidden"
             animate="visible"
             className="w-[30vw] h-[30vw] absolute top-[20%] rounded-full bg-transparent backdrop-blur-md z-20 flex box-shadow justify-center items-center">
             <NavbarComponent/>
           </motion.div>
-          {hasScrolled&&
-          <div className="w-[15vw] h-screen bg-transparent backdrop-blur-md z-10 box-shadow "></div>
-          }
-          <motion.div style={{x}} className="flex relative h-screen w-[350vw]">
+          {hasScrolled && <div className="w-[15vw] h-screen absolute bg-transparent backdrop-blur-md z-10 box-shadow "></div> }
+          <motion.div style={{x}} ref={refreshRef} className="flex relative h-screen w-[350vw]">
         
           {/* <div className="w-[500px] h-screen bg-red-100"></div>  */}
           <Homepage/>
